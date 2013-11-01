e67844cd5953294a857cfc3368ed263d53df6177
Christian Lupien <Christian.Lupien@usherbrooke.ca>
2013-10-31 21:20:51 -0400

    instruments_lecroy: fix generalization bug for StructureImproved

--- instruments_lecroy.py
+++ instruments_lecroy.py
@@ -273,7 +273,7 @@ class StructureImproved(Structure):
     Note that it can be initialized with positional arguments or keyword arguments.
     And changed later with update.
     """
-    _names_cache = []
+    _names_cache = [] # every sub class needs to have its own cache
     def _get_names(self):
         if self._names_cache == []:
             self._names_cache.extend([n for n,t in self._fields_])
@@ -316,6 +316,7 @@ class StructureImproved(Structure):
             return ret
 
 class time_stamp(StructureImproved):
+    _names_cache = [] # every sub class needs to have its own cache
     _fields_=[("seconds", c_double),
               ("minutes", c_int8),
               ("hours", c_int8),
@@ -329,6 +330,7 @@ class time_stamp(StructureImproved):
                                               self.minutes, self.seconds)
 
 class WAVEDESC(StructureImproved):
+    _names_cache = [] # every sub class needs to have its own cache
     _pack_ = 2
     _fields_ = [("DESCRIPTOR_NAME", c_char*16),
               ("TEMPLATE_NAME", c_char*16),
@@ -391,13 +393,16 @@ class WAVEDESC(StructureImproved):
         return [(name, getattr(self,name)) for name, t in self._fields_]
 
 class USERTEXT(StructureImproved):
+    _names_cache = [] # every sub class needs to have its own cache
     _fields_ = [("TEXT", c_char*160)]
 
 class TRIGTIME(StructureImproved):
+    _names_cache = [] # every sub class needs to have its own cache
     _fields_ = [("TRIGGER_TIME", c_double),
               ("TRIGGER_OFFSET", c_double)]
 
 class RISTIME(StructureImproved):
+    _names_cache = [] # every sub class needs to have its own cache
     _fields_ = [("RIS_OFFSET", c_double)]
 
 # The structure has an int which is the 0 based index into the following lists
