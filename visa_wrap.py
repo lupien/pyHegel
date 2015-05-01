6b28fd74576e9ed5ff4411341a8e141ea9283ec4
Christian Lupien <Christian.Lupien@usherbrooke.ca>
2015-04-30 20:00:40 -0400

    visa_wrap: complete coding (and some testing)

--- visa_wrap.py
+++ visa_wrap.py
@@ -9,8 +9,9 @@ Created on Wed Apr 08 15:42:32 2015
 @author: Christain lupien
 """
 
-import threading
-# don't export os
+# don't export threading, warnings, os
+import threading as _threading
+import warnings as _warnings
 import os as _os
 
 try_agilent_first = True
@@ -20,7 +21,6 @@ version = "1.4"
 
 _agilent_visa = False
 
-# TODO read/write and redirect_instr handling of delete
 
 try:
     import pyvisa
@@ -90,7 +90,7 @@ def _patch_pyvisa():
         #print 'Skipping patch: already applied'
         return
     #print 'Installing pyvisa patch'
-    visa.warnings._filters_lock_pyvisa = threading.Lock()
+    visa.warnings._filters_lock_pyvisa = _threading.Lock()
     def removefilter(*arg, **kwarg):
         #print 'Doing remove filter %r, %r'%(arg, kwarg)
         with visa.warnings._filters_lock_pyvisa:
@@ -116,7 +116,10 @@ def _old_load_visa(path):
         For Agilent: r'c:\Windows\system32\agvisa32.dll'
           (That is for agilent and NI installed at the same time)
     """
-    old_close = vpp43.visa_library().viClose
+    if vpp43.visa_library._VisaLibrary__lib:
+        old_close = vpp43.visa_library().viClose
+    else:
+        old_close = None
     if _os.name == 'nt' and path is None and try_agilent_first:
         try:
             vpp43.visa_library.load_library(agilent_path)
@@ -150,9 +153,9 @@ def _old_load_visa(path):
         visa.resource_manager.resource_name
     except visa.VisaIOError: # resource manager session is wrong.
         try:
-            visa.resource_manager.close()
+            old_close(visa.resource_manager.session)
         except visa.VisaIOError:
-            pass
+            _warnings.warn('Unable to properly close previous resource manager')
         visa.resource_manager.init()
 
     _patch_pyvisa()
@@ -164,26 +167,111 @@ def _old_load_visa(path):
 
 class redirect_instr(object):
     def __init__(self, instr_instance):
+        # Need to set this first, all others set will probably need this.
         self.instr = instr_instance
     # we redirect all unknown access to self.instr
+    def __attr_in_redirect(self, name):
+        try:
+            super(redirect_instr, self).__getattribute__(name)
+            return True
+        except AttributeError:
+            return False
     def __getattr__(self, name):
         return getattr(self.instr, name)
     def __setattr__(self, name, value):
-        if name == 'instr' or not hasattr(self.instr, name):
-            super(old_Instrument, self).__setattr__(self, name, value)
+        # To overide a value only present in self.instr, create the
+        # attribute wirh _new_attr below
+        if self.__attr_in_redirect(name):
+            # when present in self, use that
+            super(redirect_instr, self).__setattr__(name, value)
+        elif name == 'instr' or not hasattr(self.instr, name):
+            # for 'instr' or if not already in self.instr
+            # we need to check for instr because otherwise self.instr will fail
+            # on the initial self.instr =  ...
+            super(redirect_instr, self).__setattr__(name, value)
         else:
+            # it is only in self.instr
             setattr(self.instr, name, value)
+    def _new_attr(self, name, value=None):
+        super(redirect_instr, self).__setattr__(name, value)
     def __delattr__(self, name):
-        if not hasattr(self.instr, name):
-            super(old_Instrument, self).__delattr__(self, name)
+        if self.__attr_in_redirect(name):
+            super(redirect_instr, self).__delattr__(name)
         else:
             delattr(self.instr, name)
 
 ####################################################################
 # Instruments wrapper classes
 ####################################################################
+def _strip_termination(input_str, termination=None):
+    """ Similar to the one in pyvisa 1.4, except only removes CR, LF or CR+LF
+        if present at the end of the string when None is used.
+    """
+    CR = '\r'
+    LF = '\n'
+    if termination:
+        if input_str.endswith(termination):
+            return input_str[:-len(termination)]
+        else:
+            _warnings.warn("read string doesn't end with "
+                           "termination characters", stacklevel=2)
+    if input_str.endswith(CR+LF):
+        return input_str[:-2]
+    elif input_str[-1] in (CR, LF):
+        return input_str[:-1]
+    return input_str
+
+def _write_helper(self, message, termination='default'):
+    # For old: improved termination handling, does not use the delay, matches new interface
+    # For new: overides the resource write to remove handling of encoding
+    termination = self.write_termination if termination == 'default' else termination
+    if termination:
+        if message.endswith(termination):
+            _warnings.warn("write message already ends with "
+                          "termination characters", stacklevel=2)
+        else:
+            message += termination
+    self.write_raw(message)
+
+def _read_helper(self, termination='default'):
+    # For old: improved termination handling, matches new interface
+    # For new: overides the resource read to remove handling of encoding and add stripping of termination
+    #          It does not change the hardware termination handling
+    termination = self.read_termination if termination == 'default' else termination
+    return _strip_termination(self.read_raw(), termination)
+
 
 class old_Instrument(redirect_instr):
+    CR = '\r'
+    LF = '\n'
+    _read_termination = None
+    _write_termination = CR + LF
+    def __init__(self, instr_instance, **kwargs):
+        super(self, old_Instrument).__init__(instr_instance)
+        for k,v in kwargs.items():
+            setattr(self, k, v)
+    @property
+    def read_termination(self):
+        return self._read_termination
+    @read_termination.setter
+    def read_termination(self, value):
+        if value:
+            last_char = value[-1:]
+            if last_char in value[:-1]:
+                raise ValueError("ambiguous ending in termination characters")
+            # This changes the hardware read termination
+            if value is None:
+                # This is needed for serial because  VI_ATTR_TERMCHAR can also be used by VI_ATTR_ASRL_END_IN
+                # so return it to the default value
+                self.term_chars = self.LF
+            self.term_chars = value
+        self._read_termination = value
+    @property
+    def write_termination(self):
+        return self._write_termination
+    @write_termination.setter
+    def write_termination(self, value):
+        self._write_termination = value
     def is_serial(self):
         return isinstance(self.instr, visa.SerialInstrument)
     def is_gpib(self):
@@ -208,6 +296,11 @@ class old_Instrument(redirect_instr):
         vpp43.disable_event(self.vi, event_type, mechanism)
     def control_ren(self, mode):
         vpp43.gpib_control_ren(self.vi, mode)
+    # read_raw is ok
+    def write_raw(self, message):
+        vpp43.write(self.vi, message)
+    write = _write_helper
+    read = _read_helper
 
 class new_Instrument(redirect_instr):
     def is_serial(self):
@@ -220,10 +313,6 @@ class new_Instrument(redirect_instr):
         self.assert_trigger()
     def lock(self, lock_type, timeout_ms): # timeout in ms
         self.visalib.lock(self.session, lock_type, timeout_ms)
-    def enable_event(self, event_type, mechanism):
-        self.visalib.enable_event(self.session, event_type, mechanism)
-    def disable_event(self, event_type, mechanism):
-        self.visalib.disable_event(self.session, event_type, mechanism)
     def install_visa_handler(self, event_type, handler, user_handle):
         # returns the converted user_handle
         return self.visalib.install_visa_handler(self.session, event_type, handler, user_handle)
@@ -238,24 +327,43 @@ class new_Instrument(redirect_instr):
                     break
             else:
                 raise pyvisa.errors.UnknownHandler(event_type, handler, user_handle)
-            self.uninstall_handler(event_type, element[2], user_handle)            
+            self.visalib.uninstall_handler(event_type, element[2], user_handle)
         else:
-            self.visalib.uninstall_handler(self.session, event_type, handler, user_handle)
+            self.visalib.uninstall_visa_handler(self.session, event_type, handler, user_handle)
+    if version in ['1.5', '1.6', '1.6.1', '1.6.2', '1.6.3']:
+        def enable_event(self, event_type, mechanism):
+            self.visalib.enable_event(self.session, event_type, mechanism)
+        def disable_event(self, event_type, mechanism):
+            self.visalib.disable_event(self.session, event_type, mechanism)
+        @property
+        def read_termination(self):
+            return self.instr.read_termination
+        @read_termination.setter
+        def read_termination(self, value):
+            if value is None:
+                # This is needed for serial because  VI_ATTR_TERMCHAR can also be used by VI_ATTR_ASRL_END_IN
+                # so return it to the default value
+                self.instr.read_termination = '\n'
+            self.instr.read_termination = value
     def control_ren(self, mode):
         try:
             self.instr.control_ren(mode)
         except AttributeError:
             self.visalib.viGpibControlREN(self.session, mode)
+    #read_raw, write_raw are ok
+    write = _write_helper
+    read = _read_helper
+
 
 
 ####################################################################
 # Resource_manager wrapper classes
 ####################################################################
 
-def _get_resource_info_helper(self, resource_name):
+def _get_resource_info_helper(rsrc_manager, resource_name):
     normalized = alias_if_exists = None
     try:
-        _, _, _, normalized, alias_if_exists = self.resource_info(resource_name)
+        _, _, _, normalized, alias_if_exists = rsrc_manager.resource_info(resource_name)
     except AttributeError:
         pass
     return normalized, alias_if_exists
@@ -282,8 +390,8 @@ def _get_instrument_list(self, use_aliases=True):
         #  The open function is a lot less sensitive on both.
         #  according to specs, the comparison should be case insensitive.
         # So lets try a few different ones
-        normalized1, alias_if_exists1 = self.resource_info(resource_name)
-        normalized2, alias_if_exists2 = self.resource_info(resource_name.upper())
+        normalized1, alias_if_exists1 = _get_resource_info_helper(self, resource_name)
+        normalized2, alias_if_exists2 = _get_resource_info_helper(self, resource_name.upper())
         if alias_if_exists1:
             normalized, alias_if_exists = normalized1, alias_if_exists1
         elif alias_if_exists2:
@@ -296,8 +404,9 @@ def _get_instrument_list(self, use_aliases=True):
             result.append(normalized[:-7])
     return result
 
+
 class old_resource_manager(object):
-    def __init__(self):
+    def __init__(self, path=None):
         """
         Note that loading a new resource_manager will break a previous one if it used
         a different dll. It is not possible to use 2 different dlls at the same time
@@ -325,8 +434,21 @@ class old_resource_manager(object):
     get_instrument_list = _get_instrument_list
 
     def open_resource(self, resource_name, **kwargs):
+        if 'term_chars' in kwargs:
+            # does not map to
+            raise ValueError('term_chars is not permitted in open_resource')
+        kwargs_after = {}
+        for k in ['read_termination', 'write_termination']:
+            if kwargs.has_key(k):
+                kwargs_after[k] = kwargs.pop(k)
         instr = visa.instrument(resource_name, **kwargs)
-        return old_Instrument(instr)
+        if instr.is_serial():
+            # serials was setting term_chars to CR
+            # change it to same defaults as new code (which is the general default)
+            if kwargs_after == {}:
+                instr.read_termination = None
+                instr.write_termination = instr.CR+instr.LF
+        return old_Instrument(instr, **kwargs_after)
     def is_agilent(self):
         if self._is_agilent is None:
             try:
@@ -337,8 +459,8 @@ class old_resource_manager(object):
 
 
 class new_WrapResourceManager(redirect_instr):
-    def __init__(self):
-        super(new_WrapResourceManager, self).__init__()
+    def __init__(self, new_rsrc_manager):
+        super(new_WrapResourceManager, self).__init__(new_rsrc_manager)
         self._is_agilent = None
     if version in ['1.5', '1.6', '1.6.1', '1.6.2', '1.6.3']:
         # The same as the original ResourceManager one (1.6.1) except for the close statement
