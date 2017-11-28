84662aa025a92a3f7b1ae9bc3c1e80cf8e070eaf
Christian Lupien <Christian.Lupien@usherbrooke.ca>
2017-11-28 11:00:09 -0500

    scipy_fortran_fix: Fix CTRL-C problem with anaconda 5.0.1

--- pyHegel/scipy_fortran_fix.py
+++ pyHegel/scipy_fortran_fix.py
@@ -8,6 +8,14 @@
 # due to Fortran libraries
 # See stackoverflow for explanation:
 # http://stackoverflow.com/questions/15457786/ctrl-c-crashes-python-after-importing-scipy-stats
+#
+# In newer versions of scipy (at least the one with anaconda 5.0.1 as of 2017-11-27)
+# the scipy config files contains mkl_rt instead of mkl_blas.
+# The code below does work when making the change but another option is available to prevent
+# the fortran routines from setting the handler: set the environment variable FOR_DISABLE_CONSOLE_CTRL_HANDLER
+# to TRUE
+#  see https://software.intel.com/en-us/node/691984 (intel fortran 16.0 compiler, default console event handling)
+
 
 from __future__ import absolute_import
 
@@ -27,6 +35,8 @@ if os.path.exists(config_file):
         text = fid.read()
     if 'mkl_blas' in text:
         INSTALL = True
+#    if 'mkl_rt' in text:
+#        INSTALL = True
 
 def handler(sig):
     try:
@@ -46,7 +56,8 @@ def load_lib(name):
     except WindowsError:
         ctypes.CDLL(name)
 
-if INSTALL:
+def fix_problem():
+    global basepath, routine
     # load numpy math and fortran libraries (but do not import numpy)
     basepath = imp.find_module('numpy')[1]
 
@@ -67,3 +78,15 @@ if INSTALL:
     routine = ctypes.WINFUNCTYPE(ctypes.c_int, ctypes.c_uint)(handler)
 
     ctypes.windll.kernel32.SetConsoleCtrlHandler(routine, 1)
+
+def fix_problem_new(force=False):
+    envvar = 'FOR_DISABLE_CONSOLE_CTRL_HANDLER'
+    if not force:
+        if envvar in os.environ:
+            return
+    os.environ[envvar] = 'TRUE'
+
+if INSTALL:
+    fix_problem()
+else:
+    fix_problem_new()
