e58a13139843852a268453641c3095eb8fa9fc7f
Christian Lupien <Christian.Lupien@usherbrooke.ca>
2023-09-22 20:34:15 -0400

    Alazartech: python3 compatibility

--- pyHegel/instruments/Alazartech.py
+++ pyHegel/instruments/Alazartech.py
@@ -1,4 +1,28 @@
-from __future__ import absolute_import
+# -*- coding: utf-8 -*-
+
+########################## Copyrights and license ############################
+#                                                                            #
+# Copyright 2022-2023  Antoine Cornillot                                     #
+#                      Christian Lupien <christian.lupien@usherbrooke.ca>    #
+#                                                                            #
+# This file is part of pyHegel.  http://github.com/lupien/pyHegel            #
+#                                                                            #
+# pyHegel is free software: you can redistribute it and/or modify it under   #
+# the terms of the GNU Lesser General Public License as published by the     #
+# Free Software Foundation, either version 3 of the License, or (at your     #
+# option) any later version.                                                 #
+#                                                                            #
+# pyHegel is distributed in the hope that it will be useful, but WITHOUT     #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or      #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public        #
+# License for more details.                                                  #
+#                                                                            #
+# You should have received a copy of the GNU Lesser General Public License   #
+# along with pyHegel.  If not, see <http://www.gnu.org/licenses/>.           #
+#                                                                            #
+##############################################################################
+
+from __future__ import absolute_import, print_function, division
 
 import numpy as np
 import sys
@@ -692,7 +716,7 @@ class ATSBoard(BaseInstrument):
             self.board_info = board_info
 
         except:
-                raise IndexError, 'The device requested is not there.'
+                raise IndexError('The device requested is not there.')
         # Board
         board = ats.Board(systemId, cardId)
         self._board = board
